faccb91880d354a09e87b79bd934de44d130975f
KaioHerculano <164403155+KaioHerculano@users.noreply.github.com>
2025-02-10 21:49:16 -0400

    I created the base template and started the brand template

--- brands/urls.py
+++ brands/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import brand_list_view, brand_create_view, brand_delete_view
+from . import views
+
 
 urlpatterns = [
-    path('', brand_list_view, name='brand_list'),
-    path('create/', brand_create_view, name='brand_create'),
-    path('<int:pk>/delete/', brand_delete_view, name='brand_delete'),
+    path('brands/list/', views.BrandListView.as_view(), name='brand_list'),
 ]
---
+++ brands/views.py
@@ -0,0 +1,7 @@
+from django.views.generic import ListView
+from . import models
+
+class BrandListView(ListView):
+    model = models.Brand
+    template_name = 'brand_list.html'
+    context_object_name = 'brands'
