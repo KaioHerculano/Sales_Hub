b1d921ddb8499e2ad091f653408986db1f5c4bbe
Kaio <kaioherculano12@gmail.com>
2025-05-13 17:50:25 -0400

    Remove class UserProfileAdmin

--- companies/admin.py
+++ companies/admin.py
@@ -53,18 +53,6 @@ class CompanyAdmin(admin.ModelAdmin):
         return f"Empresa: {obj.name}"
     get_group.short_description = "Grupo Associado"
 
-"""
-@admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'company', 'is_company_admin')
-    search_fields = ('user__username', 'company__name')
-    list_filter = ('company', 'is_company_admin')
-
-    def get_groups(self, obj):
-        return ", ".join([group.name for group in obj.user.groups.all()])
-    get_groups.short_description = "Grupos do Usuário"
-"""
-
 
 class CustomGroupAdmin(GroupAdmin):
     def get_queryset(self, request):
