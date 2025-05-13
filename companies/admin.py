3682a6aab3c9dd1261798614bd633ecec6b07626
Kaio <kaioherculano12@gmail.com>
2025-05-13 17:47:54 -0400

    A bug in the company admin has been fixed, now after creating the company I will be able to add all users from each company directly to it in the admin.

--- companies/admin.py
+++ companies/admin.py
@@ -1,19 +1,59 @@
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin, GroupAdmin
 from companies.models import Company, UserProfile
-from django.contrib.auth.models import Group
-from django.contrib.auth.admin import GroupAdmin
+
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'Perfil do Usuário'
+    fk_name = 'user'
+
+
+class CustomUserAdmin(BaseUserAdmin):
+    inlines = (UserProfileInline,)
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return []
+        return super().get_inline_instances(request, obj)
+
+
+class UserProfileCompanyInline(admin.TabularInline):
+    model = UserProfile
+    extra = 1
+    fields = ('user', 'is_company_admin')
+    can_delete = True
+    show_change_link = True
+
+    def get_formset(self, request, obj=None, **kwargs):
+        formset = super().get_formset(request, obj, **kwargs)
+
+        class CustomFormSet(formset):
+            def __init__(self, *args, **kwargs):
+                super().__init__(*args, **kwargs)
+                for form in self.forms:
+                    if not form.instance.pk:
+                        form.fields['user'].queryset = User.objects.filter(profile__company__isnull=True)
+
+        return CustomFormSet
+
+
+
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
-    list_display = ('name', 'tax_id', 'is_active', 'email')
-    search_fields = ('name', 'tax_id', 'email')
+    list_display = ('name', 'chpj', 'is_active', 'email')
+    search_fields = ('name', 'chpj', 'email')
     list_filter = ('is_active',)
+    inlines = [UserProfileCompanyInline]
 
     def get_group(self, obj):
         return f"Empresa: {obj.name}"
     get_group.short_description = "Grupo Associado"
 
-
+"""
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'company', 'is_company_admin')
@@ -23,11 +63,16 @@ class UserProfileAdmin(admin.ModelAdmin):
     def get_groups(self, obj):
         return ", ".join([group.name for group in obj.user.groups.all()])
     get_groups.short_description = "Grupos do Usuário"
+"""
+
 
 class CustomGroupAdmin(GroupAdmin):
     def get_queryset(self, request):
         queryset = super().get_queryset(request)
         return queryset.exclude(name__startswith="Empresa:")
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
 
 admin.site.unregister(Group)
 admin.site.register(Group, CustomGroupAdmin)
--- companies/models.py
+++ companies/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Company(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    tax_id = models.CharField(max_length=10, blank=True, null=True)
+    chpj = models.CharField(max_length=14, blank=True, null=True)
     is_active = models.BooleanField(default=True)
     address = models.CharField(max_length=255, blank=True, null=True)
     phone = models.CharField(max_length=20, blank=True, null=True)
